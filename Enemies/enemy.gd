3f72008816f858cc9db71e1791d7c2ac0f36a353
ahsansuryana <ahsansuryana.12252006@gmail.com>
2026-01-01 15:06:11 +0700

    adding raycast to enemy

--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 @export var max_range = 104
 @export var speed = 50
-
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
 # Called when the node enters the scene tree for the first time.
@@ -12,14 +13,15 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var state = playback.get_current_node()
 	match state:
 		"IdleState" : pass
 		"ChaseState" : 
 			var player = get_palyer()
 			if player is Player:
-				velocity = global_position.direction_to(player.global_position) * 75
+				velocity = global_position.direction_to(player.global_position) * speed
+				sprite_2d.scale.x =  sign(velocity.x)
 			else :
 				velocity = Vector2.ZERO
 			move_and_slide()
@@ -35,3 +37,11 @@ func is_player_in_range() -> bool:
 		if player_distance < max_range:
 			result = true
 	return result
+
+func can_see_player() -> bool:
+	if not is_player_in_range(): return false
+	var player = get_palyer()
+	ray_cast_2d.target_position = player.global_position - global_position
+	ray_cast_2d.force_raycast_update()
+	var has_los_to_player: = not ray_cast_2d.is_colliding()
+	return has_los_to_player
